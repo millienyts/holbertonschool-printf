cda83d0fa146f33d0aa2bb685ec96c46b777f633
millienytorressanta <tmillieny@gmail.com>
2023-11-16 14:37:06 -0400

    print.f

---
+++ _printf.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdarg.h>
+/**
+ * _printf - Custom printf function
+ * @format: Format specifier
+ *
+ * Return: Number of characters printed (excluding null byte)
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int i, s_count, count = 0;
+
+	va_list args;
+
+	va_start(args, format);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+		}
+		else if (format[i + 1] == 'c')
+		{
+			_putchar(va_arg(args, int));
+			i++;
+		}
+		else if (format[i + 1] == 's')
+		{
+			s_count = putss(va_arg(args, char *));
+			i++;
+			count += (s_count - 1);
+		}
+		else if (format[i + 1] == '%')
+		{
+			_putchar('%');
+		}
+		count += 1;
+	}
+
+	va_end(args);
+	return (count);
+}
