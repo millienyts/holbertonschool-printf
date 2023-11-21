2c70b137961497f1cc034234c6407bc6bd2cfa59
Artiz58 <artiz58@gmail.com>
2023-11-21 14:38:13 -0400

    C - printf

--- _p.c
+++ _p.c
@@ -6,9 +6,9 @@ int print_pointer(va_list args)
 {
 	void *ptr = va_arg(args, void *);
 	char buffer[20];
-	int l = p_l(ptr);
 
 	p_to_hex(ptr, buffer);
+	write(1, buffer, _strlen(buffer));
 
-	return write(1, buffer, l);
+	return _strlen(buffer);
 }
--- _str.c
+++ _str.c
@@ -5,10 +5,10 @@
 int print_int(va_list args)
 {
 	int value = va_arg(args, int);
-	char buffer[20];
-	int l = int_l(value);
+	char buffer[12];
+	int l = int_l(value, buffer);
 
-	_str(value, buffer);
+	write(1, buffer, l);
 
-	return write(1, buffer, l);
+	return (l);
 }
