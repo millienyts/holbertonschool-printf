3f4bbe7b5c4018cc79fb187376e6c49f101c7ea9
Artiz58 <artiz58@gmail.com>
2023-11-22 13:50:38 -0400

    C - printf

--- _lphex.c
+++ _lphex.c
@@ -9,22 +9,23 @@ int p_l(void *ptr __attribute__((unused)))
 
 void p_to_hex(void *ptr, char *buffer) {
     const int p_size = sizeof(void *) * 2;
+    int firstNonZeroNibble = -1;
     int i;
-    int lZ = 1;
-
-    buffer[p_size + 1] = '\0';
 
     buffer[0] = '0';
     buffer[1] = 'x';
 
     for (i = 0; i < p_size; ++i) {
         int nibble = ((uintptr_t)ptr >> ((p_size - i - 1) * 4)) & 0xF;
-        
-        if (nibble != 0 || !lZ) {
-            buffer[i + 2] = (nibble < 10) ? ('0' + nibble) : ('a' + nibble - 10);
-            lZ = 0;
-        } else {
-            buffer[i + 2] = ' ';
+
+        if (nibble != 0 && firstNonZeroNibble == -1) {
+            firstNonZeroNibble = i;
+        }
+
+        if (firstNonZeroNibble != -1) {
+            buffer[i + 2 - firstNonZeroNibble] = (nibble < 10) ? ('0' + nibble) : ('a' + nibble - 10);
         }
     }
+
+    buffer[p_size - firstNonZeroNibble + 2] = '\0';
 }
